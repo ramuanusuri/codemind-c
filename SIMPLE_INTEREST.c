464b6105af345e88849378c6bb67b7726ecfebac
ramuanusuri <99881295+ramuanusuri@users.noreply.github.com>
2022-02-28 11:32:46 +0530

    #include<stdio.h> int main() {     int p,t,r,simple_interest;     scanf("%d%d%d",&p,&t,&r);     simple_interest=(p*t*r)/100;     printf ("%d",simple_interest);     return 0; }

---
+++ SIMPLE_INTEREST.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int p,t,r,simple_interest;
+    scanf("%d%d%d",&p,&t,&r);
+    simple_interest=(p*t*r)/100;
+    printf ("%d",simple_interest);
+    return 0;
+}
+
+
+
+
